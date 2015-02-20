e37b42943bbbef09008fe2f7d86dbb6daa057403
Wojciech Kicior <Wojciech.Kicior@gmail.com>
2015-02-20 19:44:46 +0100

    working post

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,5 @@
-FROM ingensi/oracle-jdk
-
-RUN yum update -y && yum install -y unzip
-RUN curl -O http://downloads.typesafe.com/typesafe-activator/1.2.10/typesafe-activator-1.2.10.zip
-RUN unzip typesafe-activator-1.2.10.zip -d / && rm typesafe-activator-1.2.10.zip && chmod a+x /activator-1.2.10/activator
-ENV PATH $PATH:/activator-1.2.10
-
+FROM peelsky/sbt:0.13.7
+MAINTAINER Wojciech Kicior <wkicior@github>
 EXPOSE 9000
-RUN mkdir /app
-WORKDIR /app
-CMD ["activator", "run"]
+CMD sbt ~re-start
 
--- src/main/scala/com/example/MyService.scala
+++ src/main/scala/com/example/MyService.scala
@@ -3,7 +3,11 @@ package com.example
 import akka.actor.Actor
 import spray.routing._
 import spray.http._
+import spray.json._
+import spray.httpx._
 import MediaTypes._
+
+
 
 // we don't implement our route structure directly in the service actor because
 // we want to be able to test it independently, without having to spin up an actor
@@ -19,34 +23,34 @@ class MyServiceActor extends Actor with MyService {
   def receive = runRoute(myRoute)
 }
 
+case class Person(name: String, favoriteNumber: Int)
+
+object AdsJsonProtocol extends DefaultJsonProtocol with SprayJsonSupport {
+  implicit val personFormat = jsonFormat2(Person)
+}
+
 
 // this trait defines our service behavior independently from the service actor
 trait MyService extends HttpService {
-
+  import AdsJsonProtocol._
   val myRoute =
     pathPrefix("ads") {
       get {
-          complete {
-            //TO getter
-            case class Contact(email: String)
-            case class User(id: Int, name: String, contact: Contact)
-            case class Ad(id:Int, title: String, desc: String, owner:User)
-            val ads = Ad(id=1, title="loremxxxxyyy2",desc="ipsum", owner=User(id=1, name="Dolor", contact = Contact(email="lorem@ipsum.pl")))::
+        complete {
+          //TO getter
+          case class Contact(email: String)
+          case class User(id: Int, name: String, contact: Contact)
+          case class Ad(id:Int, title: String, desc: String, owner:User)
+          val ads = Ad(id=1, title="loremxxxxyyy33",desc="ipsum", owner=User(id=1, name="Dolor", contact = Contact(email="lorem@ipsum.pl")))::
             Ad(id=2, title="lorem",desc="ipsum", owner=User(id=1, name="Dolor", contact = Contact(email="lorem@ipsum.pl")))::
             Ad(id=3, title="lorem",desc="ipsum", owner=User(id=1, name="Dolor", contact = Contact(email="lorem@ipsum.pl")))::
             Nil
-            ads.toString
-          }
+          ads.toString
+        }
       } ~
-      get {
-        respondWithMediaType(`text/html`) { // XML is marshalled to `text/xml` by default, so we simply override here
-          complete {
-            <html>
-              <body>
-                <h1>Say hello to <i>spray-routing</i> on <i>spray-can</i>!</h1>
-              </body>
-            </html>
-          }
+      post {
+        entity(as[Person]) { person =>
+          complete(s"Person: ${person.name} - favorite number: ${person.favoriteNumber}")          
         }
       }
     }
