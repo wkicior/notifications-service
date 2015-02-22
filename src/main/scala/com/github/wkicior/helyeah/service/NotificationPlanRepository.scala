069784ae06ffec17d1aa26ed48e148a0fa954c19
Wojciech Kicior <Wojciech.Kicior@gmail.com>
2015-02-22 20:35:37 +0100

    some fixes

--- src/main/scala/com/github/wkicior/helyeah/service/NotificationPlanRepository.scala
+++ src/main/scala/com/github/wkicior/helyeah/service/NotificationPlanRepository.scala
@@ -1,16 +1,16 @@
 package com.github.wkicior.helyeah.service
 
 import akka.actor.Actor
-import akka.actor.Actor.Receive
 import akka.event.Logging
 import com.github.wkicior.helyeah.model.NotificationPlan
 
 object GetNotificationPlans
 
 /**
+ * NotificationPlanRepository for fetching the NotificationPlans
  * Created by disorder on 22.02.15.
  */
-class NotificationRepository extends Actor {
+class NotificationPlanRepository extends Actor {
   val log = Logging(context.system, this)
 
   def receive = {
