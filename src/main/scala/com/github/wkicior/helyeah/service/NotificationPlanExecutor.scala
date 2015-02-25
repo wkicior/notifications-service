295d8528f24f753a1433b31285c9336a5924b1dc
Wojciech Kicior <Wojciech.Kicior@gmail.com>
2015-02-25 20:55:20 +0100

    refactoring case on forecast rating

--- src/main/scala/com/github/wkicior/helyeah/service/NotificationPlanExecutor.scala
+++ src/main/scala/com/github/wkicior/helyeah/service/NotificationPlanExecutor.scala
@@ -29,31 +29,22 @@ class NotificationPlanExecutor(forecastJudgeProps: Props, notificationComposerPr
   val log = Logging(context.system, this)
   val forecastJudge = context.actorOf(forecastJudgeProps)
 
-  val SURF_RATING_TRIGGER: Int = 2
-
   def executeNotificationPlan(message: NotificationPlanExecutorMessage) {
     log.info("executing message: ${message}")
     implicit val timeout = Timeout(100 milliseconds)
     val forecastRatingFuture = forecastJudge ? message
     val forecastRating = Await.result(forecastRatingFuture, timeout.duration).asInstanceOf[ForecastRating]
-    val rateInt: Int = getRatingAsInt(forecastRating)
-    if (rateInt >= SURF_RATING_TRIGGER) {
-      log.debug("It's a surf time")
-      val notificationComposer = context.actorOf(notificationComposerProps)
-      notificationComposer ! new NotificationComposerMessage(message.notificationPlan, forecastRating)
-    } else {
-      log.info("No wind this time")
+    forecastRating.rating match {
+      case Rating.NONE | Rating.POOR => log.info("No wind this time")
+      case Rating.PROMISING | Rating.HIGH => sendNotificationToComposer(message, forecastRating)
+      case default => log.error("Unrecognized rating ${default}")
     }
   }
 
-  def getRatingAsInt(forecastRating: ForecastRating): Int = {
-    val rateInt = forecastRating.rating match {
-      case Rating.NONE => 0
-      case Rating.POOR => 1
-      case Rating.PROMISING => 2
-      case Rating.HIGH => 3
-    }
-    rateInt
+  def sendNotificationToComposer(message: NotificationPlanExecutorMessage, forecastRating: ForecastRating): Unit = {
+    log.debug("It's a surf time")
+    val notificationComposer = context.actorOf(notificationComposerProps)
+    notificationComposer ! new NotificationComposerMessage(message.notificationPlan, forecastRating)
   }
 
   def receive = {
